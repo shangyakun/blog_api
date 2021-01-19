a04330d13d6fce8befc0db79a4aa609c33f7b89b
shangyakun <shangyakun@yingyinglicai.com>
2021-01-19 12:59:52 +0800

    blog_strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '114ab2ad183e5bd8431f4151acfc824a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '11c4a1ed-fc45-4f72-ab3c-1bb4b04e2d2b'
+};
